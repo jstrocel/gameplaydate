3efb8d3962d3bf37f5628d7a794f67b9b0141dfd
James Strocel <James.Strocel@gmail.com>
2013-01-25 20:56:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gameplaydate::Application.config.secret_token = 'fad5c93e644bb8c4737d3fa2416af150c28c79a2d9a7f72c51d221d59398aad835fd59ab6d9c527b4ad0fbbb2d687c1037dfb3abc9d7082dc2a09ebab2d86328'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gameplaydate::Application.config.session_store :cookie_store, key: '_gameplaydate_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gameplaydate::Application.config.session_store :active_record_store
