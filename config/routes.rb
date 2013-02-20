96e8aef695eb1d53f6a7ea7036a0b5381cfe0aaa
James Strocel <James.Strocel@gmail.com>
2013-02-19 23:24:50 -0800

    more red tests for the user paces spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Gameplaydate::Application.routes.draw do
-  get "static_pages/home"
+  resources :users
+
+   root to: 'static_pages#home'
+
+   match '/signup',  to: 'users#new'
+ 
 
   get "static_pages/help"
 
-  get "users/new"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
