bc5e1cef89b0ca8c9104c5a11cfbb2592247c8eb
James Strocel <James.Strocel@gmail.com>
2013-09-10 15:21:05 -0700

    switching e-mail provider to sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Gameplaydate::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
+/ActionMailer::Base.smtp_settings = {
   :address              => "mail.gameplaydate.com",
   :port                 => 587,
   :domain               => "gameplaydate.com",
@@ -12,4 +12,14 @@ ActionMailer::Base.smtp_settings = {
   :password             => "wo4ghu4luru5to",
   :authentication       => :login,
   :enable_starttls_auto => false
+}/
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['jtstrocel'],
+  :password       => ENV['sha0roth6tu8gi'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
